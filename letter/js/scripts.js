a725c3421b57395365d7b68b78ada64b244b83e0
Joanna Anderson and Jason Downing <jason@>
2016-08-10 11:44:01 -0700

    Fix selector form input issue. Now the form will display select form response

--- letter/js/scripts.js
+++ letter/js/scripts.js
@@ -2,12 +2,12 @@
 $(document).ready(function() {
 
   $("#blanks form").submit(function(event) {
-    var input1 = $("input#input1").val();
-    var input2 = $("input#input2").val();
-    var input3 = $("input#input3").val();
-    var input4 = $("input#input4").val();
-    var input5 = $("input#input5").val();
-    var input6 = $("input#input6").val();
+    var input1 = $("#input1").val();
+    var input2 = $("#input2").val();
+    var input3 = $("#input3").val();
+    var input4 = $("#input4").val();
+    var input5 = $("#input5").val();
+    var input6 = $("#input6").val();
 
     $(".input1").text(input1);
     $(".input2").text(input2);
